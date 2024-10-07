3fd08569c42fcbdc8d4affb3cbfb691db1100d00
JS-Debug <168356324+JS-Debug@users.noreply.github.com>
2024-10-07 14:37:29 +0100

    ///

--- components/adLibrary/microComponents/PageNameWithPopover.tsx
+++ components/adLibrary/microComponents/PageNameWithPopover.tsx
@@ -88,6 +88,13 @@ const PageNameWithPopover: React.FC<PageNameWithPopoverProps> = ({
           <div className="mt-4 flex justify-center space-x-2">
             {page_profile_uri && (
               <>
+                <Link href={`/dashboard/adlibrary/${page_id}`}>
+                  <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-teal-300 to-lime-300 p-0.5 text-sm font-medium text-gray-900 focus:outline-none focus:ring-4 focus:ring-lime-200 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 dark:focus:ring-lime-800">
+                    <span className="relative rounded-md bg-white px-5 py-1 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
+                      View Ads
+                    </span>
+                  </button>
+                </Link>
                 <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-cyan-500 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-cyan-200 group-hover:from-cyan-500 group-hover:to-blue-500 dark:text-white dark:focus:ring-cyan-800">
                   <span className="relative rounded-md bg-white px-5 py-1 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
                     <a
@@ -99,13 +106,6 @@ const PageNameWithPopover: React.FC<PageNameWithPopoverProps> = ({
                     </a>
                   </span>
                 </button>
-                <Link href={`/dashboard/adlibrary/${page_id}`}>
-                  <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-teal-300 to-lime-300 p-0.5 text-sm font-medium text-gray-900 focus:outline-none focus:ring-4 focus:ring-lime-200 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 dark:focus:ring-lime-800">
-                    <span className="relative rounded-md bg-white px-5 py-1 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
-                      View Ads
-                    </span>
-                  </button>
-                </Link>
               </>
             )}
           </div>
